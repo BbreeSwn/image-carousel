fdb7f67bdfcfeeeb1906be66a8c6ecf18e575fa1
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-02 16:45:07 +0700

    feat : set image data

---
+++ src/components/ImageData.js
@@ -0,0 +1,24 @@
+const ImageData = [
+    {
+        title: "When the sun down",
+        image: 'https://cdn.pixabay.com/photo/2019/11/23/07/24/christmas-4646421_1280.jpg'
+    },
+    {
+        title: "vitamin SEA",
+        image: 'https://cdn.pixabay.com/photo/2022/04/06/12/41/landscape-7115513_1280.jpg'
+    },
+    {
+        title: "Relaxing with Waterfall",
+        image: 'https://cdn.pixabay.com/photo/2023/03/11/18/47/waterfall-7845023_1280.jpg'
+    },
+    {
+        title: "Smile like sun flower",
+        image: 'https://cdn.pixabay.com/photo/2016/11/23/14/48/sunflowers-1853323_1280.jpg'
+    },
+    {
+        title: "Amazing view",
+        image: 'https://cdn.pixabay.com/photo/2023/05/22/13/45/mount-fuji-8010752_1280.jpg'
+    },
+]
+
+export default ImageData
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import ImageData from "./ImageData";
 
 const ImageSlider = () => {
   return (
-    <div>ImageSlider</div>
-  )
-}
+    <section className="slider" >
+      {ImageData.map((data, index) => {
+        return (
+          <div className="slide" key={index}>
+            <div>
+                <img src={data.image} alt={data.title}/>
+                <p>{data.title}</p>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
 
-export default ImageSlider
+export default ImageSlider;
