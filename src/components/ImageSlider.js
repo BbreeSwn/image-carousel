870e41c5fab5c71e69cdc7bc81c30be83e9f5c14
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-03 10:54:00 +0700

    feat : add arrow

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ImageData from "./ImageData";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
 
 const ImageSlider = () => {
   return (
     <section className="slider" >
+        <FaArrowLeft className="arrowLeft" />
+        <FaArrowRight className="arrowRight" />
       {ImageData.map((data, index) => {
         return (
           <div className="slide" key={index}>
+
             <div>
                 <img src={data.image} alt={data.title} className='image' />
                 <p>{data.title}</p>
@@ -14,6 +18,7 @@ const ImageSlider = () => {
           </div>
         );
       })}
+      
     </section>
   );
 };
