5d68317f18b26960a89f6b920e8c6978b900e5f1
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-03 11:30:18 +0700

    feat : setCurrent state

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,24 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import ImageData from "./ImageData";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const ImageSlider = () => {
-  return (
-    <section className="slider" >
-        <FaArrowLeft className="arrowLeft" />
-        <FaArrowRight className="arrowRight" />
-      {ImageData.map((data, index) => {
-        return (
-          <div className="slide" key={index}>
+  const [current, setCurrent] = useState(0);
 
+  const prevSlide = () => {
+    setCurrent(current - 1);
+  };
+  const nextSlide = () => {
+    setCurrent(current + 1);
+  };
+
+  return (
+    <section className="slider">
+      <FaArrowLeft className="arrowLeft" onClick={prevSlide} />
+      <FaArrowRight className="arrowRight" onClick={nextSlide} />
+      {ImageData.map((data, index) => {
+
+        return (
+          <div className={index === current? "slide active" : 'slide'} key={index}>
             <div>
-                <img src={data.image} alt={data.title} className='image' />
-                <p>{data.title}</p>
+              <img src={data.image} alt={data.title} className="image" />
+              <p>{data.title}</p>
             </div>
           </div>
         );
       })}
-      
     </section>
   );
 };
